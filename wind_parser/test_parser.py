8112e8007fda5095befb2d33d3db2928ec95ec9a
Njoguu <njoguuallan@gmail.com>
2023-01-14 12:25:45 +0300

    refactor from using unittest to pytest. closes #2

--- wind_parser/test_parser.py
+++ wind_parser/test_parser.py
@@ -1,39 +1,36 @@
 import sys
-import unittest
+import pytest
 from .parser import Parser, Argument
 
 
-class parserTestCase(unittest.TestCase):
-    def test_separate_arguments(self):
-        sys.argv = ['python', '--name=Anthony', '--age=16', '--verbose', '--list',
-                    'Paul', 'Célia', 'Mathieu', '--logging', '-l', 'this', 'for', 'while']
+# Test separate_arguments method of Parser class
+def test_separate_arguments():
+    sys.argv = ['python', '--name=Anthony', '--age=16', '--verbose', '--list',
+                'Paul', 'Célia', 'Mathieu', '--logging', '-l', 'this', 'for', 'while']
 
-        p = Parser(sys.argv)
+    p = Parser(sys.argv)
 
-        self.assertEqual(p.separate_args(),
-                         ['--name=Anthony', '--age=16', '--verbose', '--list', ['Paul', 'Célia', 'Mathieu'], '--logging', '-l', ['this', 'for', 'while']])
+    assert p.separate_args() == ['--name=Anthony', '--age=16', '--verbose', '--list', ['Paul', 'Célia', 'Mathieu'], '--logging', '-l', ['this', 'for', 'while']]    
 
-    def test_parse_values(self):
-        sys.argv = ['python', '--name=Anthony', '--age=16', '--verbose',
-                    '--list', 'Paul', 'Célia', 'Mathieu', '--logging', '-l', 'this', 'for', 'while']
-        p = Parser(sys.argv)
 
-        self.assertEqual(p.args,
-                         {'name': 'Anthony', 'age': '16', 'verbose': True, 'list': ['Paul', 'Célia', 'Mathieu'], 'logging': True, 'l': ['this', 'for', 'while']})
+# Test the parse_values method of the Parser class
+def test_parse_values():
+    sys.argv = ['python', '--name=Anthony', '--age=16', '--verbose',
+                '--list', 'Paul', 'Célia', 'Mathieu', '--logging', '-l', 'this', 'for', 'while']
+    p = Parser(sys.argv)
 
-        sys.argv.extend(['--test1', '--test2', '-z','16'])
+    assert p.args == {'name': 'Anthony', 'age': '16', 'verbose': True, 'list': ['Paul', 'Célia', 'Mathieu'], 'logging': True, 'l': ['this', 'for', 'while']}
 
-        p = Parser(sys.argv)
+    sys.argv.extend(['--test1', '--test2', '-z','16'])
 
-        self.assertEqual(p.args,
-                         {'name': 'Anthony', 'age': '16', 'verbose': True, 'list': ['Paul', 'Célia', 'Mathieu'], 'logging': True, 'l': ['this', 'for', 'while'],'test1':True, 'test2':True, 'z':'16'})
+    p = Parser(sys.argv)
 
-class argumentTestCase(unittest.TestCase):
-    def test_argument(self):
-        self.assertTrue(Argument("-v").is_key())
-        self.assertTrue(Argument("--help").is_key())
-        self.assertTrue(Argument("-a=16").is_kwarg())
-        self.assertTrue(Argument([1,2,3]).is_value())
+    assert p.args == {'name': 'Anthony', 'age': '16', 'verbose': True, 'list': ['Paul', 'Célia', 'Mathieu'], 'logging': True, 'l': ['this', 'for', 'while'],'test1':True, 'test2':True, 'z':'16'}
 
-if __name__ == '__main__':
-    unittest.main()
+
+# Test the Argument class
+def test_argument():
+    assert Argument("-v").is_key()
+    assert Argument("--help").is_key()
+    assert Argument("-a=16").is_kwarg()
+    assert Argument([1,2,3]).is_value()
