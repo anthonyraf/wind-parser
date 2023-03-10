feb9d717007af3e51248d6383f94efccb17db283
Anthony Rafidison <100955619+anthonyraf@users.noreply.github.com>
2023-03-10 15:04:15 +0000

    Use new folder for the updated parser

---
+++ wind_parser/parser/core.py
@@ -0,0 +1,12 @@
+import sys
+from typing import Any, Dict, List, Union
+from .parser import ArgParser
+
+
+class Parser:
+    def __init__(self, arguments: List[str]):
+        self.__argparser = ArgParser()
+        self.args = self.__argparser.parse(arguments)
+
+
+
